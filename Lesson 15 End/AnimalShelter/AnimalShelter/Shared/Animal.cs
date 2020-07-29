8032e1f257fb3e0cab64322cd1a901ac2b79733b
Johan Smarius <jsmarius@hotmail.com>
2020-07-29 15:44:58 +0200

    Added some folders

---
+++ Lesson 15 End/AnimalShelter/AnimalShelter/Shared/Animal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AnimalShelter.Shared
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                if (DateOfBirth.HasValue)
+                {
+                    var calculatedAge = DateTime.Today.Year - DateOfBirth.Value.Year;
+
+                    if (DateTime.Today.DayOfYear < DateOfBirth.Value.DayOfYear)
+                    {
+                        calculatedAge -= 1;
+                    }
+
+                    return calculatedAge;
+                }
+
+                return EstimatedAge.Value;
+            }
+        }
+
+        public int? EstimatedAge { get; set; }
+
+        public AnimalKind AnimalKind { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public Gender Gender { get; set; }
+    }
+}
